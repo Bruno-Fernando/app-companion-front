f600b8e5557e2d16c84e7716b79180e7025eb2fe
Bruno Fernando da Silva <bfs@cesar.org.br>
2022-10-24 13:14:38 -0300

    Update login connection

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,6 +1,7 @@
 import GoogleIcon from '@mui/icons-material/Google';
-import { Box, Button } from '@mui/material';
-import { GoogleAuthProvider, signInWithPopup, User } from 'firebase/auth';
+import { Alert, Box, Button, Snackbar } from '@mui/material';
+import axios, { AxiosError } from 'axios';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -8,19 +9,44 @@ import auth from '../../Shared/Services/Firebase';
 import { saveToken } from '../../Shared/Utils/Auth';
 
 function Login() {
+  const [open, setOpen] = useState(false);
+  const [alertMessage, setAlertMessage] = useState('');
   const navigate = useNavigate();
 
   const handleGoogleSignIn = () => {
     const provider = new GoogleAuthProvider();
 
     signInWithPopup(auth, provider)
-      .then((result) => {
-        saveToken(JSON.stringify(result.user.toJSON()));
-        navigate('/home');
+      .then(async (result) => {
+        const token = await result.user.getIdToken();
+
+        try {
+          await axios.get(`${import.meta.env.VITE_BASE_URL}user/verify`, {
+            headers: {
+              authorization: `Bearer ${token}`,
+            },
+          });
+
+          saveToken(token);
+          navigate('/home');
+        } catch (error) {
+          const err = error as AxiosError;
+          setOpen(true);
+          if (err.response?.status === 404) {
+            setAlertMessage('Usuário não cadastrado');
+          } else {
+            setAlertMessage('Aconteceu um erro, tente novamente mais tarde');
+          }
+        }
       })
       .catch((error) => {
-        console.log(error);
+        setOpen(true);
+        setAlertMessage('Aconteceu um erro, tente novamente mais tarde');
       });
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   return (
@@ -40,6 +66,12 @@ function Login() {
       >
         Entrar com Google
       </Button>
+
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+          {alertMessage}
+        </Alert>
+      </Snackbar>
     </Box>
   );
 }
